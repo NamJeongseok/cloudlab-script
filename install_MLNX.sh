5f67295ee71bdeaa43b5e4dc64bf5dd84bd4dae9
NamJeongseok <130465059+NamJeongseok@users.noreply.github.com>
2024-12-09 14:18:51 +0900

    Update install_MLNX.sh

--- install_MLNX.sh
+++ install_MLNX.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install MLNX OFED Firmware.
+# Install MLNX OFED Firmware
 wget https://content.mellanox.com/ofed/MLNX_OFED-4.9-5.1.0.0/MLNX_OFED_LINUX-4.9-5.1.0.0-ubuntu18.04-x86_64.tgz
 tar -xvf MLNX_OFED_LINUX-4.9-5.1.0.0-ubuntu18.04-x86_64.tgz
 cd MLNX_OFED_LINUX-4.9-5.1.0.0-ubuntu18.04-x86_64
@@ -8,3 +8,7 @@ cd MLNX_OFED_LINUX-4.9-5.1.0.0-ubuntu18.04-x86_64
 sudo ./mlnxofedinstall  --force
 sudo /etc/init.d/openibd restart
 sudo /etc/init.d/opensmd restart
+
+cd ..
+sudo rm -rf ./MLNX_OFED_LINUX-4.9-5.1.0.0-ubuntu18.04-x86_64
+sudo rm ./MLNX_OFED_LINUX-4.9-5.1.0.0-ubuntu18.04-x86_64.tgz
